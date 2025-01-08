86df0cdcc1c499e270612193223189b325914c67
Abuzar Khan <abuzarkhan1242@gmail.com>
2025-01-08 18:43:05 +0500

    updated

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import {
   Code2,
   Cpu,
@@ -13,7 +13,6 @@ import {
   Bot,
   Users,
   Lock,
-  Unlock,
 } from 'lucide-react';
 
 const SkillMatrix = () => {
